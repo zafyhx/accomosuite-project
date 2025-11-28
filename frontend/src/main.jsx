67d57ec5fb535b1d34a0fec04e3706b7f40305d5
zafyhx <zafysterofficialtm@gmail.com>
2025-11-29 03:55:41 +0700

    fix: set global axios base url in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,9 @@ import App from "./App.jsx";
 import ScrollToTop from "./components/ScrollToTop";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
